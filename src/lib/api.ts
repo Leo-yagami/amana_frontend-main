89d822af3f41768cd398e87bc5262d5b47603733
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-06 14:44:45 +0300

    axios revert changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -65,7 +65,6 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-axios.defaults.withCredentials = true;
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
