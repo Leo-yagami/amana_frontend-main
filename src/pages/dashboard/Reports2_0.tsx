5aeeb59f20d75387928efb8f785df3484e9062c9
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-10 12:10:19 +0300

    threshold fix

--- src/pages/dashboard/Reports2_0.tsx
+++ src/pages/dashboard/Reports2_0.tsx
@@ -558,7 +558,7 @@ const ReportsAnalytics = () => {
   const { t } = useTranslation();
   const [range, setRange] = useState<RangeType>("6m");
   const [activeTab, setActiveTab] = useState("summary");
-  const { ref, isInView } = useInView({ threshold: 2 });
+  const { ref, isInView } = useInView({ threshold: 1 });
 
   // const {
   //   data,
