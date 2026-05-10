b8a4d379fa5f4f0c9291b28694827c182a1988f4
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-10 11:53:29 +0300

    changing the in view threshold

--- src/pages/dashboard/Reports2_0.tsx
+++ src/pages/dashboard/Reports2_0.tsx
@@ -558,7 +558,7 @@ const ReportsAnalytics = () => {
   const { t } = useTranslation();
   const [range, setRange] = useState<RangeType>("6m");
   const [activeTab, setActiveTab] = useState("summary");
-  const { ref, isInView } = useInView({ threshold: 0.5 });
+  const { ref, isInView } = useInView({ threshold: 0.3 });
 
   // const {
   //   data,
