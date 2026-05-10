eb698c9a1faf9e3a6ce20ae3de61923ab6572a00
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-10 12:13:51 +0300

    threshold testing 2

--- src/pages/dashboard/Reports2_0.tsx
+++ src/pages/dashboard/Reports2_0.tsx
@@ -558,7 +558,7 @@ const ReportsAnalytics = () => {
   const { t } = useTranslation();
   const [range, setRange] = useState<RangeType>("6m");
   const [activeTab, setActiveTab] = useState("summary");
-  const { ref, isInView } = useInView({ threshold: 1 });
+  const { ref, isInView } = useInView({ threshold: 0.001 });
 
   // const {
   //   data,
