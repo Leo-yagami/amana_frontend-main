49ce693466a43ac56e83381d3066b4fe20ad5a1d
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-06-07 11:06:43 +0300

    loader change on protected routes + theme preservation(light/dark mode) + FOUC elimination

--- src/components/ProtectedRoutePayment.tsx
+++ src/components/ProtectedRoutePayment.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
-import { Skeleton } from '@/components/ui/skeleton';
+// import { Skeleton } from '@/components/ui/skeleton';
+import { Loader2} from "lucide-react";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -11,12 +12,13 @@ export const ProtectedRoute2 = ({ children }: ProtectedRouteProps) => {
 
   if (isLoading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="space-y-4 w-full max-w-md p-8">
+      <div className="min-h-screen bg-background flex items-center justify-center">
+        {/* <div className="space-y-4 w-full max-w-md p-8"> */}
+          {/* <Skeleton className="h-12 w-full" />
           <Skeleton className="h-12 w-full" />
-          <Skeleton className="h-12 w-full" />
-          <Skeleton className="h-12 w-full" />
-        </div>
+          <Skeleton className="h-12 w-full" /> */}
+          <Loader2 className="w-10 h-10 animate-spin text-primary" />
+        {/* </div> */}
       </div>
     );
   }
