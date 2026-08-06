188cd882ce8ad92af39daaf4d7706dedd81a7bfa
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-08-06 14:00:52 +0300

    Update Index1.tsx

--- src/pages/Index1.tsx
+++ src/pages/Index1.tsx
@@ -1,4 +1,4 @@
-import HeroSection from "@/components/landing/HeroSection6";
+import HeroSection from "@/components/landing/HeroSection7";
 import AboutSection from "@/components/landing/AboutSection1";
 import StatsSection from "@/components/landing/StatsSection1";
 import CampaignsSection from "@/components/landing/CampaignsSection1";
